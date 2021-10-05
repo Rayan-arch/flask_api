846de35f5ee63757764eb2516d2ca0bf2eac31b9
Rayan <greg139@wp.pl>
2021-10-05 17:27:26 +0200

    initial commit

--- db.py
+++ db.py
@@ -1,13 +1,21 @@
 import psycopg2
 from flask import g
+from os import getenv
+from dotenv import load_dotenv
 
+load_dotenv()
 
 def init_app(app):
     app.teardown_appcontext(close_connection)
 
 def get_connection():
     if 'connection' not in g:
-        g.connection = psycopg2.connect(dbname='app',user='admin1', password='admin123', host='db')
+        g.connection = psycopg2.connect(
+            dbname=getenv('DB_NAME', 'app'),
+            user=getenv('DB_USER', 'admin1'),
+            password=getenv('DB_PASSWORD', 'admin123'),
+            host=getenv('DB_HOST', 'db')
+        )
 
     return g.connection
 
